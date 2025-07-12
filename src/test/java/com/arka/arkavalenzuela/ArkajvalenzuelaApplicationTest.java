73b513c0269efb3aae868d438d9f8e2902ad7b34
Jorge Koke Valenzuela <jorge.valenzuela.diaz@ciisa.cl>
2025-07-11 23:08:47 -0400

    Signed-off-by: Jorge Koke Valenzuela <jorge.valenzuela.diaz@ciisa.cl>
     Changes to be committed:
    	new file:   .gradle/8.14.3/checksums/checksums.lock
    	new file:   .gradle/8.14.3/checksums/md5-checksums.bin
    	new file:   .gradle/8.14.3/checksums/sha1-checksums.bin
    	new file:   .gradle/8.14.3/executionHistory/executionHistory.bin
    	new file:   .gradle/8.14.3/executionHistory/executionHistory.lock
    	new file:   .gradle/8.14.3/fileChanges/last-build.bin
    	new file:   .gradle/8.14.3/fileHashes/fileHashes.bin
    	new file:   .gradle/8.14.3/fileHashes/fileHashes.lock
    	new file:   .gradle/8.14.3/fileHashes/resourceHashesCache.bin
    	new file:   .gradle/8.14.3/gc.properties
    	new file:   .gradle/buildOutputCleanup/buildOutputCleanup.lock
    	new file:   .gradle/buildOutputCleanup/cache.properties
    	new file:   .gradle/buildOutputCleanup/outputFiles.bin
    	new file:   .gradle/file-system.probe
    	new file:   .gradle/vcs-1/gc.properties
    	modified:   build.gradle
    	new file:   build/classes/java/main/com/arka/arkavalenzuela/ArkajvalenzuelaApplication.class
    	new file:   build/classes/java/main/com/arka/arkavalenzuela/ServletInitializer.class
    	new file:   build/classes/java/test/com/arka/arkavalenzuela/ArkajvalenzuelaApplicationTest.class
    	new file:   build/classes/java/test/com/arka/arkavalenzuela/ArkajvalenzuelaApplicationTests.class
    	new file:   build/classes/java/test/com/arka/arkavalenzuela/ServletInitializerTest.class
    	new file:   build/libs/arkajvalenzuela-0.0.1-SNAPSHOT-plain.war
    	new file:   build/libs/arkajvalenzuela-0.0.1-SNAPSHOT.war
    	new file:   build/reports/problems/problems-report.html
    	new file:   build/reports/tests/test/classes/com.arka.arkavalenzuela.ArkajvalenzuelaApplicationTest.html
    	new file:   build/reports/tests/test/classes/com.arka.arkavalenzuela.ArkajvalenzuelaApplicationTests.html
    	new file:   build/reports/tests/test/classes/com.arka.arkavalenzuela.ServletInitializerTest.html
    	new file:   build/reports/tests/test/css/base-style.css
    	new file:   build/reports/tests/test/css/style.css
    	new file:   build/reports/tests/test/index.html
    	new file:   build/reports/tests/test/js/report.js
    	new file:   build/reports/tests/test/packages/com.arka.arkavalenzuela.html
    	new file:   build/resolvedMainClassName
    	new file:   build/resources/main/application.properties
    	new file:   build/test-results/test/TEST-com.arka.arkavalenzuela.ArkajvalenzuelaApplicationTest.xml
    	new file:   build/test-results/test/TEST-com.arka.arkavalenzuela.ArkajvalenzuelaApplicationTests.xml
    	new file:   build/test-results/test/TEST-com.arka.arkavalenzuela.ServletInitializerTest.xml
    	new file:   build/test-results/test/binary/output.bin
    	new file:   build/test-results/test/binary/output.bin.idx
    	new file:   build/test-results/test/binary/results.bin
    	new file:   build/tmp/bootWar/MANIFEST.MF
    	new file:   build/tmp/compileJava/previous-compilation-data.bin
    	new file:   build/tmp/compileTestJava/compileTransaction/stash-dir/ServletInitializerTest.class.uniqueId0
    	new file:   build/tmp/compileTestJava/previous-compilation-data.bin
    	new file:   build/tmp/war/MANIFEST.MF
    	modified:   src/main/resources/application.properties
    	new file:   src/resources/application.properties
    	new file:   src/test/java/com/arka/arkavalenzuela/ArkajvalenzuelaApplicationTest.java
    	new file:   src/test/java/com/arka/arkavalenzuela/ServletInitializerTest.java

---
+++ src/test/java/com/arka/arkavalenzuela/ArkajvalenzuelaApplicationTest.java
@@ -0,0 +1,12 @@
+package com.arka.arkavalenzuela;
+
+import org.junit.jupiter.api.Test;
+
+public class ArkajvalenzuelaApplicationTest {
+
+    @Test
+    public void testMainRunsWithoutException() {
+        // This test simply verifies that the main method runs without throwing exceptions.
+        ArkajvalenzuelaApplication.main(new String[]{});
+    }
+}
