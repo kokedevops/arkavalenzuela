d345e30689ff48a4323890fa20466d7d57b85f05
Jorge Koke Valenzuela <jorge.valenzuela.diaz@ciisa.cl>
2025-08-28 22:28:58 -0400

    elimina r2dbc
    Signed-off-by: Jorge Koke Valenzuela <jorge.valenzuela.diaz@ciisa.cl>

--- src/main/java/com/arka/arkavalenzuela/ArkajvalenzuelaApplication.java
+++ src/main/java/com/arka/arkavalenzuela/ArkajvalenzuelaApplication.java
@@ -2,9 +2,12 @@ package com.arka.arkavalenzuela;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.r2dbc.R2dbcAutoConfiguration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+    R2dbcAutoConfiguration.class
+})
 @EnableScheduling  // 🔔 Enable scheduling for abandoned cart notifications
 public class ArkajvalenzuelaApplication {
 
